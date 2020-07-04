126f6d97c21f65be412b7179a8b99ddab54fb5df
Mads Kristensen <Madsk@microsoft.com>
2020-07-04 07:58:33 -0700

    Add context menu on posts Fixes #1

--- src/DeveloperNews/ToolWindows/PostControl.xaml.cs
+++ src/DeveloperNews/ToolWindows/PostControl.xaml.cs
@@ -1,54 +1,68 @@
-﻿using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
+
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace FeedManager.ToolWindows
 {
-	public partial class PostControl : UserControl
-	{
-		private static readonly Regex _regex = new Regex(@"</?\w+((\s+\w+(\s*=\s*(?:"".*?""|'.*?'|[^'"">\s]+))?)+\s*|\s*)/?>", RegexOptions.Singleline);
-		private readonly Uri _url;
+    public partial class PostControl : UserControl
+    {
+        private static readonly Regex _regex = new Regex(@"</?\w+((\s+\w+(\s*=\s*(?:"".*?""|'.*?'|[^'"">\s]+))?)+\s*|\s*)/?>", RegexOptions.Singleline);
+        private readonly Uri _url;
 
-		public PostControl(SyndicationItem item)
-		{
-			_url = item.Links.FirstOrDefault().Uri;
+        public PostControl(SyndicationItem item)
+        {
+            _url = item.Links.FirstOrDefault().Uri;
 
-			InitializeComponent();
+            InitializeComponent();
 
-			lblTitle.Content = item.Title.Text;
-			lblSummary.Text = TruncateHtml(item.Summary.Text);
-		}
+            lblTitle.Content = item.Title.Text;
+            lblSummary.Text = TruncateHtml(item.Summary.Text);
+        }
 
-		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-		{
-			ThreadHelper.ThrowIfNotOnUIThread();
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (GeneralOptions.Instance.OpenInDefaultBrowser)
+            {
+                OpenInDefaultBrowserClick(this, null);
+            }
+            else
+            {
+                OpenInVsClick(this, null);
+            }
+        }
 
-			if (GeneralOptions.Instance.OpenInDefaultBrowser)
-			{
-				Process.Start(_url.OriginalString);
-			}
-			else
-			{
-				var service = Package.GetGlobalService(typeof(IVsWebBrowsingService)) as IVsWebBrowsingService;
-				service.Navigate(_url.OriginalString, (uint)__VSWBNAVIGATEFLAGS.VSNWB_WebURLOnly, out _);
-			}
-		}
+        public static string TruncateHtml(string input, int length = 200, string ommission = "...")
+        {
+            var clearText = _regex.Replace(input, "");
 
-		public static string TruncateHtml(string input, int length = 200, string ommission = "...")
-		{
-			var clearText = _regex.Replace(input, "");
+            var nextSpace = clearText.LastIndexOf(" ", Math.Min(length, clearText.Length));
 
-			var nextSpace = clearText.LastIndexOf(" ", Math.Min(length, clearText.Length));
+            return string.Format("{0}" + ommission,
+                                  clearText.Substring(0, (nextSpace > 0) ? nextSpace : length).Trim());
+        }
 
-			return string.Format("{0}" + ommission,
-								  clearText.Substring(0, (nextSpace > 0) ? nextSpace : length).Trim());
-		}
-	}
+        private void OpenInVsClick(object sender, RoutedEventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var service = Package.GetGlobalService(typeof(IVsWebBrowsingService)) as IVsWebBrowsingService;
+            service.Navigate(_url.OriginalString, (uint)__VSWBNAVIGATEFLAGS.VSNWB_WebURLOnly, out _);
+        }
+
+        private void OpenInDefaultBrowserClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start(_url.OriginalString);
+        }
+
+        private void CopyUrlClick(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(_url.OriginalString);
+        }
+    }
 }
