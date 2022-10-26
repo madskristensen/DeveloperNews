771a59225a4203b6fad0e515a70f0b1b359ad7f3
Mads Kristensen <madskristensen@users.noreply.github.com>
2022-10-26 10:43:14 -0700

    Changed default browser setting [release]

--- src/DeveloperNews/Settings/Options.cs
+++ src/DeveloperNews/Settings/Options.cs
@@ -10,8 +10,8 @@ namespace DevNews
         [Category("General")]
         [DisplayName("Open in default browser")]
         [Description("Specifies if the blog posts should open in the default browser or inside Visual Studio.")]
-        [DefaultValue(false)]
-        public bool OpenInDefaultBrowser { get; set; }
+        [DefaultValue(true)]
+        public bool OpenInDefaultBrowser { get; set; } = true;
 
         [DefaultValue("")]
         [Browsable(false)]
