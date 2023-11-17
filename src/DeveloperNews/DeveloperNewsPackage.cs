a7c2e17342bf9a337c0c253d380be46265e0b4aa
Mads Kristensen <madskristensen@users.noreply.github.com>
2023-11-17 14:37:00 -0800

    The most significant changes involve the addition of a new status bar icon and associated functionality in the `DeveloperNewsPackage.cs` file, and the creation of a new `StatusBarInjector.cs` file. The version number of the extension has also been updated in both `source.extension.cs` and `source.extension.vsixmanifest` files.
    1. In `DeveloperNews.csproj`, a new source file `StatusBarInjector.cs` has been added to the project.
    2. The `DeveloperNewsPackage.cs` file has undergone several changes. New `using` directives have been added for various namespaces. The `PackageRegistration` attribute has been moved and a new `ProvideAutoLoad` attribute has been added. Several methods have been added or modified, including `InitializeAsync`, `SetupStatusBarIconAsync`, `Panel_MouseEnter`, `StatusIconClicked`, `GetAsyncToolWindowFactory`, and `GetToolWindowTitle`. These changes seem to add a new status bar icon and associated functionality.
    3. The version number of the extension has been updated from `1.0` to `1.0.900` in `source.extension.cs`.
    4. In `source.extension.vsixmanifest`, the version number in the `Identity` tag has been updated to `1.0.900`, matching the change in `source.extension.cs`. No other changes have been made; the existing lines have been reformatted.
    5. A new file `StatusBarInjector.cs` has been added. This file contains a new `StatusBarInjector` class with methods for finding a child element in the visual tree, ensuring the UI is ready, and injecting a control into the status bar.

--- src/DeveloperNews/DeveloperNewsPackage.cs
+++ src/DeveloperNews/DeveloperNewsPackage.cs
@@ -2,9 +2,16 @@
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using DevNews.Resources;
 using DevNews.ToolWindows;
+using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -12,47 +19,88 @@ using Task = System.Threading.Tasks.Task;
 
 namespace DevNews
 {
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(PackageGuids.guidDeveloperNewsPackageString)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
     [ProvideProfile(typeof(DialogPageProvider.General), Vsix.Name, "General", 0, 110, false)]
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [ProvideToolWindow(typeof(NewsWindow), Style = VsDockStyle.Tabbed, Window = ToolWindowGuids.SolutionExplorer)]
     [ProvideToolWindowVisibility(typeof(NewsWindow), VSConstants.UICONTEXT.NoSolution_string)]
     [ProvideToolWindowVisibility(typeof(NewsWindow), VSConstants.UICONTEXT.SolutionHasSingleProject_string)]
     [ProvideToolWindowVisibility(typeof(NewsWindow), VSConstants.UICONTEXT.SolutionHasMultipleProjects_string)]
     [ProvideToolWindowVisibility(typeof(NewsWindow), VSConstants.UICONTEXT.EmptySolution_string)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class DeveloperNewsPackage : AsyncPackage
     {
         public static FeedStore Store { get; private set; }
 
-        /// <summary>
-        /// The entry point for the extension. This is executed when the tool window is activated.
-        /// </summary>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            SetupStatusBarIconAsync().FileAndForget(nameof(DevNews));
+
             await NewsWindowCommand.InitializeAsync(this);
+        }
+
+        private async Task SetupStatusBarIconAsync()
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            DockPanel panel = new()
+            {
+                Margin = new Thickness(0, 0, 5, 0),
+                Width = 24,
+                Height = 22,
+                ToolTip = "Open Developer News (Ctrl+Alt+N)",
+            };
+
+            CrispImage img = new()
+            {
+                Moniker = KnownMonikers.Dictionary,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Width = 16,
+                Height = 16,
+            };
+
+            panel.Children.Add(img);
+            panel.MouseUp += StatusIconClicked;
+            panel.MouseEnter += Panel_MouseEnter;
+            panel.MouseLeave += Panel_MouseEnter;
+
+            await StatusBarInjector.InjectControlAsync(panel);
+        }
+
+        private void Panel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var panel = (Panel)sender;
+
+            if (panel.IsMouseOver)
+            {
+                panel.Background = new SolidColorBrush(Colors.White) { Opacity = 0.1 };
+            }
+            else
+            {
+                panel.Background = Brushes.Transparent;
+            }
+        }
+
+        private void StatusIconClicked(object sender, MouseButtonEventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = GetService(typeof(DTE)) as DTE2;
+            dte.Commands.Raise(PackageGuids.guidDeveloperNewsPackageCmdSetString, PackageIds.NewsWindowCommandId, null, null);
         }
 
         public override IVsAsyncToolWindowFactory GetAsyncToolWindowFactory(Guid toolWindowType)
         {
-            if (toolWindowType.Equals(PackageGuids.guidToolWindow))
-            {
-                return this;
-            }
-
-            return null;
+            return toolWindowType.Equals(PackageGuids.guidToolWindow) ? this : null;
         }
 
         protected override string GetToolWindowTitle(Type toolWindowType, int id)
         {
-            if (toolWindowType == typeof(NewsWindow))
-            {
-                return Text.WindowTitle;
-            }
-
-            return base.GetToolWindowTitle(toolWindowType, id);
+            return toolWindowType == typeof(NewsWindow) ? Text.WindowTitle : base.GetToolWindowTitle(toolWindowType, id);
         }
 
         protected override async Task<object> InitializeToolWindowAsync(Type toolWindowType, int id, CancellationToken cancellationToken)
---
+++ src/DeveloperNews/StatusBarInjector.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Shell;
+
+namespace DevNews
+{
+    internal static class StatusBarInjector
+    {
+        private static Panel _panel;
+
+        private static DependencyObject FindChild(DependencyObject parent, string childName)
+        {
+            if (parent == null)
+            {
+                return null;
+            }
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+
+            for (int i = 0; i < childrenCount; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is FrameworkElement frameworkElement && frameworkElement.Name == childName)
+                {
+                    return frameworkElement;
+                }
+
+                child = FindChild(child, childName);
+
+                if (child != null)
+                {
+                    return child;
+                }
+            }
+
+            return null;
+        }
+
+        private static async Task EnsureUIAsync()
+        {
+            while (_panel is null)
+            {
+                _panel = FindChild(Application.Current.MainWindow, "StatusBarPanel") as DockPanel;
+                if (_panel is null)
+                {
+                    // Start window is showing. Need to wait for status bar render.
+                    await Task.Delay(5000);
+                }
+            }
+        }
+
+        public static async Task InjectControlAsync(FrameworkElement element)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            await EnsureUIAsync();
+
+            element.SetValue(DockPanel.DockProperty, Dock.Right);
+            _panel?.Children.Insert(1, element);
+        }
+    }
+}
